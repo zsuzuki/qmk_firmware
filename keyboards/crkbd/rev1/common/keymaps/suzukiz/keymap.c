3f427b5f24c2af557480084084d230cfbe420936
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2020-12-27 01:35:05 +0900

    corne自分用キーマップ。

---
+++ keyboards/crkbd/rev1/common/keymaps/suzukiz/keymap.c
@@ -0,0 +1,192 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include <keymap_jp.h>
+
+enum layer_number {
+    _QWERTY = 0,
+    _RAISE,
+    _LOWER,
+    _ADJUST,
+};
+
+#define KC_BLSF RSFT_T(JP_BSLS)
+#define MC_BSLH A(JP_YEN)
+#define KC_RASPC LT(_RAISE, KC_SPC)
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define ADJUST MO(_ADJUST)
+#define KC_C_BS LCTL_T(KC_BSPC)  // ctrl
+#define KC_S_SP RSFT_T(KC_SPC)  // shift
+#define KC_Z_SH LSFT_T(KC_Z)  // shift
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, KC_C_BS,   LOWER,   KC_RASPC, KC_RSFT, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [1] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,JP_MKANA, XXXXXXX, KC_HOME, KC_PGUP,  KC_TAB,                      JP_COLN,  KC_ENT,   KC_UP,   JP_AT, JP_TILD, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,JP_MEISU, XXXXXXX,  KC_END, KC_PGDN,C(KC_TAB),                     JP_PIPE, KC_LEFT, KC_DOWN, KC_RGHT, KC_BLSF, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LSFT, _______,  ADJUST,    _______, _______, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [2] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB, JP_DQUO, JP_HASH,  JP_DLR, JP_PERC, JP_AMPR,                      JP_QUOT, JP_LPRN, JP_RPRN, JP_CIRC,  JP_EQL, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL, JP_EXLM, XXXXXXX, XXXXXXX,  KC_DEL,  KC_ESC,                      JP_ASTR, JP_MINS, JP_LBRC, JP_RBRC, JP_PLUS,  KC_GRV,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       JP_YEN, MC_BSLH, JP_LABK, JP_RABK, JP_UNDS, KC_TILD,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, _______, _______,     ADJUST, _______, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [3] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, _______, _______,    _______, _______, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  )
+};
+// clang-format on
+
+#ifdef OLED_DRIVER_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (!is_master) {
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
+    return rotation;
+}
+
+#    define L_BASE 0
+#    define L_LOWER 2
+#    define L_RAISE 4
+#    define L_ADJUST 8
+
+void oled_render_layer_state(void) {
+    oled_write_P(PSTR("Layer: "), false);
+    switch (layer_state) {
+        case L_BASE:
+            oled_write_ln_P(PSTR("Default"), false);
+            break;
+        case L_LOWER:
+            oled_write_ln_P(PSTR("Lower"), false);
+            break;
+        case L_RAISE:
+            oled_write_ln_P(PSTR("Raise"), false);
+            break;
+        case L_ADJUST:
+        case L_ADJUST | L_LOWER:
+        case L_ADJUST | L_RAISE:
+        case L_ADJUST | L_LOWER | L_RAISE:
+            oled_write_ln_P(PSTR("Adjust"), false);
+            break;
+    }
+}
+
+char keylog_str[24] = {};
+
+const char code_to_name[60] = {' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\', '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+
+void set_keylog(uint16_t keycode, keyrecord_t *record) {
+    char name = ' ';
+    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) || (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
+        keycode = keycode & 0xFF;
+    }
+    if (keycode < 60) {
+        name = code_to_name[keycode];
+    }
+
+    // update keylog
+    snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c", record->event.key.row, record->event.key.col, keycode, name);
+}
+
+void oled_render_keylog(void) { oled_write_ln(keylog_str, false); }
+
+const char *rgb_mode_list[] = {
+    "OFF", "STATIC", "BREATHING", "BREATHING 1", "BREATHING 2", "BREATHING 3", "RAINBOW MOOD", "RAINBOW MOOD 1", "RAINBOW MOOD 2", "RAINBOW SWIRL", "RB SWIRL 1", "RB SWIRL 2", "RB SWIRL 3", "RB SWIRL 4", "RB SWIRL 5", "SNAKE", "SNAKE 1", "SNAKE 2", "SNAKE 3", "SNAKE 4", "SNAKE 5", "KNIGHT", "KNIGHT 1", "KNIGHT 2", "CHRISTMAS", "GRADIENT", "GRADIENT 1", "GRADIENT 2", "GRADIENT 3", "GRADIENT 4", "GRADIENT 5", "GRADIENT 6", "GRADIENT 7", "GRADIENT 8", "GRADIENT 9", "RGB TEST", "ALTERNATING", "TWINKLE", "TWINKLE 1", "TWINKLE 2", "TWINKLE 3", "TWINKLE 4", "TWINKLE 5",
+};
+
+void oled_render_rgbmode(void) {
+    char buff[32];
+    int  idx = rgblight_get_mode();
+    int  sz  = sizeof(rgb_mode_list) / sizeof(const char *);
+    snprintf(buff, sizeof(buff), "RGB: %d,%s", idx, rgb_mode_list[idx % sz]);
+    oled_write_ln(buff, false);
+}
+
+void render_bootmagic_status(bool status) {
+    /* Show Ctrl-Gui Swap options */
+    static const char PROGMEM logo[][2][3] = {
+        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
+        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
+    };
+    if (status) {
+        oled_write_ln_P(logo[0][0], false);
+        oled_write_ln_P(logo[0][1], false);
+    } else {
+        oled_write_ln_P(logo[1][0], false);
+        oled_write_ln_P(logo[1][1], false);
+    }
+}
+
+void oled_render_logo(void) {
+    static const char PROGMEM crkbd_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
+    oled_write_P(crkbd_logo, false);
+}
+
+void oled_task_user(void) {
+    if (is_master) {
+        oled_render_layer_state();
+        oled_render_keylog();
+        oled_render_rgbmode();
+    } else {
+        oled_render_logo();
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        set_keylog(keycode, record);
+    }
+    return true;
+}
+#endif  // OLED_DRIVER_ENABLE
---
+++ keyboards/crkbd/rev1/common/keymaps/suzukiz/rules.mk
@@ -0,0 +1,5 @@
+MOUSEKEY_ENABLE     = yes    # Mouse keys
+RGBLIGHT_ENABLE     = yes     # Enable WS2812 RGB underlight.
+VIA_ENABLE          = no    # Enable VIA
+OLED_DRIVER_ENABLE  = yes
+LTO_ENABLE          = yes
